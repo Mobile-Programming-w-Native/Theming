fcc56c576c79e8610f8e9eec8f64d6dc22b3d251
OxCone1 <t1lava01@students.oamk.fi>
2023-02-08 23:41:36 +0200

    Theming (week 4)

---
+++ app/src/main/java/com/kotlin/theming/ui/theme/Theme.kt
@@ -0,0 +1,39 @@
+package com.kotlin.theming
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import com.kotlin.theming.ui.theme.*
+
+private val DarkColorPalette = darkColors(
+    primary = Orange,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = Orange,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+@Composable
+fun WalkthroughThemingTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
